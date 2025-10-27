d112005c2cae5e36e0f84720808ada64b3c932f7
mrullldhm <mrullldhm94@gmail.com>
2025-10-27 16:23:07 +0800

    Week 2 Day 1

--- week2-frontend-angular/day1/myMahir/src/app/app.routes.ts
+++ week2-frontend-angular/day1/myMahir/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home-page/home-page';
+import { CalculatorPage } from './pages/calculator-page/calculator-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'calculator', pathMatch: 'full' },
+  { path: 'home', component: HomePage },
+  { path: 'calculator', component: CalculatorPage },
+];
--- week2-frontend-angular/day1/myMahir/src/app/app.ts
+++ week2-frontend-angular/day1/myMahir/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
+  // standalone : true,
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('myMahir');
+  protected readonly title = signal('MyMahir - Agmo');
 }
